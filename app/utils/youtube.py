b1a583f53dca2b4b67a611abf84bd374813afd11
MattJeff <mhiguinen235@gmail.com>
2024-08-14 20:05:14 +0200

    infomration pour youtube

--- app/utils/youtube.py
+++ app/utils/youtube.py
@@ -1,25 +1,39 @@
 # app/utils/youtube.py
 
 import re
+import ssl
+import requests
 from pytube import YouTube
 import os
+import yt_dlp
+
+
+#desactiver le ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Désactiver la vérification des certificats SSL pour 'requests'
+requests.packages.urllib3.disable_warnings()
 
 def fetch_youtube_data(video_url):
     print("Starting YouTube data extraction")
     
     try:
-        yt = YouTube(video_url)
+     
+
+        ydl_opts = {
+        'format': 'mp4',
+        'outtmpl': '%(title)s.%(ext)s',
+                }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(video_url, download=True)
+            
+        
         youtube_data = {
-            "video_url": video_url,
-            "title": yt.title,
-            "likes": yt.views,  # Pytube ne fournit pas de likeCount directement
-            "comments": "",  # Pytube ne supporte pas les commentaires directement
-            "views": yt.views,
-            "username": yt.author,
-            "followers": "",  # Pytube ne supporte pas le comptage des abonnés
-            "following": "",  # YouTube n'a pas de concept explicite de "following"
-            "publications": "",  # Pas de moyen direct d'obtenir le nombre de vidéos de la chaîne
-            "profile_url": yt.channel_url
+        "video_url": video_url,
+        "title": info_dict.get('title', ''),
+        "views": info_dict.get('view_count', 0),
+        "username": info_dict.get('uploader', ''),
+        "profile_url": info_dict.get('uploader_url', '')
         }
 
     except Exception as e:
@@ -38,11 +52,19 @@ def extract_youtube_video_id(url):
 def download_youtube_video(video_url, output_path="."):
     print(f"Starting download of YouTube video from {video_url}")
     try:
-        yt = YouTube(video_url)
-        stream = yt.streams.get_highest_resolution()
-        video_path = stream.download(output_path=output_path)
+
+        ydl_opts = {
+            'format': 'mp4',
+            'outtmpl': '%(title)s.%(ext)s',
+                }
+        
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(video_url, download=True)
+            video_path = ydl.prepare_filename(info_dict)
+         
         print(f"Video downloaded to {video_path}")
         return video_path
+    
     except Exception as e:
         print(f"Error during video download: {e}")
         raise
